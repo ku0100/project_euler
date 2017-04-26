0856e97e7a33afc6d12a28302b5e46728a2d39a6
ku0100 <kuwilliams1@gmail.com>
2017-04-26 10:44:04 -0400

    problem 001 and empty readme

---
+++ euler001.js
@@ -0,0 +1,11 @@
+var totalSum = 0;
+
+for(i = 0; i < 1000; i++) {
+  if(i % 3 === 0 || i % 5 === 0) {
+    totalSum = totalSum + i;
+  } else {
+    totalSum = totalSum;
+  }
+}
+
+console.log(totalSum);
